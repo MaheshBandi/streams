e9dc0cb3e7d69cd3e6f111d345aa56e1540c30ca
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-29 03:37:22 +0530

    Installed and setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 import {App} from "./components/App";
-ReactDom.render(<App />, document.querySelector("#root"));
+
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+const store = createStore(reducers);
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+, document.querySelector("#root"));
