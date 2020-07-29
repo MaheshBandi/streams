5d7699adf19600bc8b8b9b7ae378a6d259d35c40
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-29 16:19:03 +0530

    Added onSubmit Handler to submit the form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -12,14 +12,18 @@ import {Field,reduxForm} from 'redux-form';
         );
      }
 
+     onSubmit(formValues){
+         console.log(formValues);
+     }
     
     render(){
         console.log(this.props);
         return(
             <div>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                     <Field  name="tittle" component={this.renderInput} label="Enter Tittle"/>
                     <Field  name="description" component={this.renderInput} label="Enter Description"/>
+                    <button className="ui button primary">submit</button>
                 </form>
             </div>
         );
